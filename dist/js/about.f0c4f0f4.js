339852036680de819e69af078e118fb127567b65
Fang <yufw0321@gmail.com>
2023-12-21 17:11:36 +0800

    add dist folder

---
+++ dist/js/about.f0c4f0f4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfang_shop"]=self["webpackChunkfang_shop"]||[]).push([[443],{833:function(n,a,s){s.r(a),s.d(a,{default:function(){return h}});var u=s(3396);const t={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),r=[e];function c(n,a){return(0,u.wg)(),(0,u.iD)("div",t,r)}var o=s(89);const f={},i=(0,o.Z)(f,[["render",c]]);var h=i}}]);
+//# sourceMappingURL=about.f0c4f0f4.js.map
